3663bc91df5bb54b4ac79ac8f23aa8003c95e095
Mateus Franco <112667339+MateusFrancoDev@users.noreply.github.com>
2025-09-02 10:29:01 -0300

    feat: Menu lateral

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,43 @@
 "use client";
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { HomeIcon, ListOrdered, LogInIcon, MenuIcon,PercentIcon,  ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetTrigger, SheetContent, SheetHeader } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
+      <Sheet>
+        <SheetTrigger asChild>
       <Button size="icon" variant="outline">
         <MenuIcon />
       </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
+        <div className="mt-2 flex flex-col gap-2">
+          <Button variant="outline" className="w-full justify-start gap-2">
+            <LogInIcon size={16}/> 
+            Login
+            </Button>
+
+            <Button variant="outline" className="w-full justify-start gap-2">
+            <HomeIcon size={16}/> 
+            Início
+            </Button>
+            
+            <Button variant="outline" className="w-full justify-start gap-2">
+            <PercentIcon size={16}/> 
+            Ofertas
+            </Button>
+            
+            <Button variant="outline" className="w-full justify-start gap-2">
+            <ListOrdered size={16}/> 
+            Catálogo
+            </Button>     
+                </div>
+      </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold">
         {" "}
